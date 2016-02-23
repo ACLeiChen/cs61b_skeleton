9e9d6e734335375eba5f91583dc9556bb1b58d2c
ACLeiChen <c1520169286@gmail.com>
2016-02-23 05:39:58 +0100

    Add a little comment in the Editor.java

--- proj2/editor/Editor.java
+++ proj2/editor/Editor.java
@@ -12,7 +12,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 //import java.util.LinkedList;
-
+//cannot use the TextFlow, TextArea, TextInputControl, or HTMLEditor classes
 
 public class Editor extends Application {
     private static final int WINDOW_WIDTH = 500;
