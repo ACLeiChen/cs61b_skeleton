c1201d22eb6ee004ae4bf610d114275b90d1d42d
ACLeiChen <c1520169286@gmail.com>
2016-02-28 16:37:12 +0100

    use linkedlist to store data, will be changed to map

--- proj2/editor/DrawCursorInTheMiddleOfTheScreen.java
+++ proj2/editor/DrawCursorInTheMiddleOfTheScreen.java
@@ -1,6 +1,8 @@
 package editor;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.VPos;
 import javafx.scene.Group;
@@ -12,49 +14,42 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.util.LinkedList;
-//cannot use the TextFlow, TextArea, TextInputControl, or HTMLEditor classes
+import javafx.util.Duration;
 
-public class Editor extends Application {
+/**
+ * Created by ChenLei on 2016/2/26.
+ */
+public class DrawCursorInTheMiddleOfTheScreen extends Application {
     private final Rectangle textBoundingBox;
+
     private static final int WINDOW_WIDTH = 500;
     private static final int WINDOW_HEIGHT = 500;
 
-    private static final int STARTING_FONT_SIZE = 20;
-    private static final int STARTING_TEXT_POSITION_X = 250;
-    private static final int STARTING_TEXT_POSITION_Y = 250;
-    private int fontSize = STARTING_FONT_SIZE;
-    private String fontName = "Verdana";
-
-    private LinkedList<Text> textList;
-    private LinkedList<Double> xrenderList;
-    private LinkedList<Double> yrenderList;
-    
-
-    public Editor() {
+    public DrawCursorInTheMiddleOfTheScreen() {
         // Create a rectangle to surround the text that gets displayed.  Initialize it with a size
         // of 0, since there isn't any text yet.
         textBoundingBox = new Rectangle(0, 0);
-        /**Storing the textList of the document. Initialize it with an empty LinkedList.*/
-        textList = new LinkedList<Text>();
-
-        
     }
+
     /** An EventHandler to handle keys that get pressed. */
     private class KeyEventHandler implements EventHandler<KeyEvent> {
         int textCenterX;
         int textCenterY;
 
-        final Group rootThis;
+        private static final int STARTING_TEXT_POSITION_X = 250;
+        private static final int STARTING_TEXT_POSITION_Y = 250;
+        private static final int STARTING_FONT_SIZE = 20;
+
         /** The Text to display on the screen. */
-        /*****
         private Text displayText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
-        *****/
+        private int fontSize = STARTING_FONT_SIZE;
+
+        private String fontName = "Verdana";
+
         KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
-            textCenterX = 0;
-            textCenterY = 0;
-            rootThis = root;
-            /***********
+            textCenterX = windowWidth / 2;
+            textCenterY = windowHeight / 2;
+
             // Initialize some empty text and add it to root so that it will be displayed.
             displayText = new Text(textCenterX, textCenterY, "");
             // Always set the text origin to be VPos.TOP! Setting the origin to be VPos.TOP means
@@ -64,10 +59,7 @@ public class Editor extends Application {
             // simpler!
             displayText.setTextOrigin(VPos.TOP);
             displayText.setFont(Font.font(fontName, fontSize));
-
-            // All new Nodes need to be added to the root in order to be displayed.
             root.getChildren().add(displayText);
-             ***********/
         }
 
         @Override
@@ -80,16 +72,11 @@ public class Editor extends Application {
                 if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
                     // Ignore control keys, which have non-zero length, as well as the backspace
                     // key, which is represented as a character of value = 8 on Windows.
-                    Text typedText = new Text(characterTyped);
-                    // add this text to the root each time when new text is typed
-                    rootThis.getChildren().add(typedText);
-                    //store this text in the textList
-                    textList.addLast(typedText);
+                    displayText.setText(characterTyped);
                     keyEvent.consume();
                 }
-                //render all the text
-                renderEngine();
-                
+
+                centerTextAndUpdateBoundingBox();
             } else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
                 // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
                 // events have a code that we can check (KEY_TYPED events don't have an associated
@@ -97,74 +84,77 @@ public class Editor extends Application {
                 KeyCode code = keyEvent.getCode();
                 if (code == KeyCode.UP) {
                     fontSize += 5;
-                    renderEngine();
+                    displayText.setFont(Font.font(fontName, fontSize));
+                    centerTextAndUpdateBoundingBox();
                 } else if (code == KeyCode.DOWN) {
                     fontSize = Math.max(0, fontSize - 5);
-                    renderEngine();
-                }
-                if (code == KeyCode.BACK_SPACE) {
-                    if (textList.size() != 0 ) {
-                        /**First remove the last text from root!*/
-                        rootThis.getChildren().remove(textList.getLast());
-                        /**then delete it from the textList.*/
-                        textList.removeLast();
-                        renderEngine();
-                    }
+                    displayText.setFont(Font.font(fontName, fontSize));
+                    centerTextAndUpdateBoundingBox();
                 }
             }
         }
 
-        /**update all the rendering information when it's called*/
-        private void renderEngine() {
-            /**clear all the existed rendering information,
-             * by initiate xrenderList and yrenderList instead of in the Editor constructor */
-            xrenderList = new LinkedList<Double>();
-            yrenderList = new LinkedList<Double>();
+        private void centerTextAndUpdateBoundingBox() {
+            // Figure out the size of the current text.
+            double textHeight = displayText.getLayoutBounds().getHeight();
+            double textWidth = displayText.getLayoutBounds().getWidth();
 
+            // Calculate the position so that the text will be centered on the screen.
+            double textTop = textCenterY - textHeight / 2;
+            double textLeft = textCenterX - textWidth / 2;
 
-            /**update the rendering information*/
-            int characterNumber = textList.size();
-            //int rowNumber = (int) Math.round((double) characterNumber / WINDOW_WIDTH);
-            if (characterNumber != 0) {
-                //for the first character in the first row
-                Text currentText = textList.getFirst();
-                // Calculate the position so that the text will be at top left on the screen.
-                double textTop = 0;
-                double textLeft = 0;
-                xrenderList.addLast(textLeft);
-                yrenderList.addLast(textTop);
+            // Re-position the text.
+            displayText.setX(textLeft);
+            displayText.setY(textTop);
 
-                setCurrentText(currentText, 0);
-                double textHeight = currentText.getLayoutBounds().getHeight();
-                double textWidth = currentText.getLayoutBounds().getWidth();
-                xrenderList.addLast(textWidth);
-                yrenderList.addLast(textTop);
-                currentText.toFront();
+            // Re-size and re-position the bounding box.
+            textBoundingBox.setHeight(textHeight);
+            textBoundingBox.setWidth(1);
 
-                double currentWidth = textWidth;
-                double currentHeight = textTop;
-                //for the rest text
-                int textIndex = 1;
-                while (textIndex < characterNumber) {
-                    setCurrentText(currentText, textIndex);
-                    textWidth = currentText.getLayoutBounds().getWidth();
-                    textHeight = currentText.getLayoutBounds().getHeight();
-                    currentWidth += textWidth;
-                    currentHeight = textTop;
-                    xrenderList.addLast(currentWidth);
-                    yrenderList.addLast(currentHeight);
-                    textIndex += 1;
-                }
-            }
+            // For rectangles, the position is the upper left hand corner.
+            textBoundingBox.setX(textLeft + textWidth);
+            textBoundingBox.setY(textTop);
+            // Many of the JavaFX classes have implemented the toString() function, so that
+            // they print nicely by default.
+            System.out.println("Bounding box: " + textBoundingBox);
+
+            // Make sure the text appears in front of the rectangle.
+            displayText.toFront();
+        }
+    }
+
+    /** An EventHandler to handle changing the color of the rectangle. */
+    private class RectangleBlinkEventHandler implements EventHandler<ActionEvent> {
+        private int currentColorIndex = 0;
+        private Color[] boxColors = {Color.BLACK, Color.TRANSPARENT};
+
+        RectangleBlinkEventHandler() {
+            // Set the color to be the first color in the list.
+            changeColor();
         }
 
-        private void setCurrentText(Text currentText, int textIndex) {
-            currentText = textList.get(textIndex);
-            currentText.setX(xrenderList.get(textIndex));
-            currentText.setY(yrenderList.get(textIndex));
-            currentText.setTextOrigin(VPos.TOP);
-            currentText.setFont(Font.font(fontName, fontSize));
+        private void changeColor() {
+            textBoundingBox.setFill(boxColors[currentColorIndex]);
+            currentColorIndex = (currentColorIndex + 1) % boxColors.length;
         }
+
+        @Override
+        public void handle(ActionEvent event) {
+            changeColor();
+        }
+    }
+
+    /** Makes the text bounding box change color periodically. */
+    public void makeRectangleColorChange() {
+        // Create a Timeline that will call the "handle" function of RectangleBlinkEventHandler
+        // every 1 second.
+        final Timeline timeline = new Timeline();
+        // The rectangle should continue blinking forever.
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        RectangleBlinkEventHandler cursorChange = new RectangleBlinkEventHandler();
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), cursorChange);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
     }
 
     @Override
@@ -184,19 +174,19 @@ public class Editor extends Application {
         scene.setOnKeyTyped(keyEventHandler);
         scene.setOnKeyPressed(keyEventHandler);
 
+        // All new Nodes need to be added to the root in order to be displayed.
+        root.getChildren().add(textBoundingBox);
+        makeRectangleColorChange();
 
-
-
-        
-        primaryStage.setTitle("Editor");
+        primaryStage.setTitle("Single Letter Display");
 
         // This is boilerplate, necessary to setup the window where things are displayed.
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
 }
+
